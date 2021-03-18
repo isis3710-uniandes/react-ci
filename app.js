618f1b0f572bd0e14003236a55b11f1835b2768f
nplata1812 <53950395+nplata1812@users.noreply.github.com>
2021-03-18 16:39:20 -0500

    Arreglar errores linter

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const express = require("express");
-const path = require('path');
+const path = require("path");
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, "build")));
 
-app.listen(PORT, () => {
-  console.log("Server started on port 5000");
-});
+app.listen(PORT, () => {});
